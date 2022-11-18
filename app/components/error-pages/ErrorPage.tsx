59865b2e60bb417adb91a305b12e7031402aaccd
Juan D. Jara <juanorigami@gmail.com>
2022-11-18 14:18:25 +0100

    polish error pages

---
+++ app/components/error-pages/ErrorPage.tsx
@@ -0,0 +1,46 @@
+import { buttonCN, cardCN } from "@/lib/style"
+import { Link, Links, Meta, Scripts } from "@remix-run/react"
+
+export default function ErrorPage({ title, details }: { title: React.ReactNode; details: React.ReactNode }) {
+  function onReload() {
+    if (typeof window !== 'undefined') {
+      window.location.reload()
+    }
+  }
+
+  return (
+    <html>
+      <head>
+        <title>Whoops 😟</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div className="h-screen flex flex-col items-center justify-center text-slate-700 text-center">
+          <p className="text-4xl">
+            <span>Whoops!</span>
+            <span className="inline-block ml-2" role='img' aria-label='Worried face'>😟</span>
+          </p>
+          <p className="mt-2">There was an error on our side, sorry.</p>
+          <p className="mt-8 mb-1">Error details: </p>
+          <div className={`${cardCN} mb-6 space-y-2 max-w-screen-sm w-full`}>
+            <p className="mt-1 text-base font-semibold">{title}</p>
+            <p className="text-xl ">{details}</p>
+          </div>
+          <div>
+            <button
+              onClick={onReload}
+              className={`mr-2 ${buttonCN.primary} ${buttonCN.normal}`}
+            >
+              Try again
+            </button>
+            <Link to="/" className={`inline-block ${buttonCN.cancel} ${buttonCN.normal}`}>
+              Go home
+            </Link>
+          </div>
+        </div>
+        <Scripts />
+      </body>
+    </html>
+  )
+}
--- app/components/error-pages/GenericCatchBoundary.tsx
+++ app/components/error-pages/GenericCatchBoundary.tsx
@@ -1,30 +1,14 @@
-import { Link, Links, Meta, Scripts, useCatch } from "@remix-run/react"
+import { useCatch, useLocation } from "@remix-run/react"
+import ErrorPage from "./ErrorPage"
 
 export default function GenericCatchBoundary() {
+  const loc = useLocation()
   const { status, statusText, data } = useCatch()
-  const title = `${status} ${statusText}`
+  const title = `HTTP ${status} ${statusText}`
 
-  return (
-    <html>
-      <head>
-        <title>{'Oops! 😟 ' + title}</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div className="h-screen flex flex-col items-center justify-center text-slate-700 text-center">
-          <p className="text-2xl">
-            <span role='img' aria-label='Worried face'>😟</span>
-            {status === 404 ? 'There is nothing here' : `I'm sorry`}
-          </p>
-          <div className="my-6">
-            <p className="text-xl font-semibold">{title}</p>
-            <p className="text-base">{typeof data === 'object' ? data?.message : data}</p>
-          </div>
-          <Link to="/" className="bg-slate-700 text-white rounded-lg px-4 py-2">Take me home</Link>
-        </div>
-        <Scripts />
-      </body>
-    </html>
-  )
+  if (status === 404) {
+    return <ErrorPage title={title} details={`There is nothing at ${loc.pathname}`} />
+  }
+
+  return <ErrorPage title={title} details={typeof data === 'object' ? data?.message : data} />
 }
