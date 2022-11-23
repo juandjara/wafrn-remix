c7d1439c8d7eccaafa21d1541a2e07ac463bc57b
Juan D. Jara <juanorigami@gmail.com>
2022-11-23 03:08:23 +0100

    file deletion layout

--- app/components/post/PostEditor.tsx
+++ app/components/post/PostEditor.tsx
@@ -12,6 +12,7 @@ import { Popover } from '@headlessui/react'
 import { buttonCN, cardCN, checkboxCN, inputCN, labelCN } from '@/lib/style'
 import { Form } from '@remix-run/react'
 import { createPortal } from 'react-dom'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 function EditorToolbar() {
   return (
@@ -71,14 +72,14 @@ function ImageUpload() {
 
   return portalRoot && createPortal(
     <Popover className="absolute top-6 right-6">
-      <Popover.Button title='Upload image or video'>
+      <Popover.Button title='Upload image or video' aria-label='Upload image or video'>
         <svg viewBox="0 0 18 18" height='18px'>
           <rect stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' height="10" width="12" x="3" y="4"></rect>
           <circle fill='currentColor' cx="6" cy="7" r="1"></circle>
           <polyline fillRule='evenodd' fill='currentColor' points="5 12 5 11 7 9 8 10 11 7 13 9 13 12 5 12"></polyline>
         </svg>
       </Popover.Button>
-      <Popover.Panel className={`absolute top-full right-0 z-10 w-72 shadow-md ${cardCN}`}>
+      <Popover.Panel className={`absolute top-full right-0 z-10 w-80 shadow-md ${cardCN}`}>
         <Form method='post' action='/api/image-upload' encType='multipart/form-data'>
           <input multiple onChange={handleFileChange} accept='image/*, video/*' type="file" className='hidden' ref={inputRef} />  
           {urls.length > 0 ? (
@@ -93,10 +94,32 @@ function ImageUpload() {
               </div>
               {urls.map(url => {
                 if (url.startsWith('data:image')) {
-                  return <img key={url} className='my-4' src={url} alt="" />
+                  return (
+                    <div className='my-4 flex items-start gap-2' key={url}>
+                      <div><img src={url} alt="" /></div>
+                      <button
+                        title='Delete file'
+                        aria-label='Delete file'
+                        onClick={() => setUrls(urls => urls.filter(u => u !== url))}
+                        className={`p-1 rounded-md ${buttonCN.delete}`}>
+                        <XMarkIcon className='w-5 h-5' />
+                      </button>
+                    </div>
+                  )
                 }
                 if (url.startsWith('data:video')) {
-                  return <video key={url} className="my-4" src={url} controls />
+                  return (
+                    <div className='my-4 flex items-start gap-2' key={url}>
+                      <div><video src={url} controls /></div>
+                      <button
+                        title='Delete file'
+                        aria-label='Delete file'
+                        onClick={() => setUrls(urls => urls.filter(u => u !== url))}
+                        className={`p-1 rounded-md ${buttonCN.delete}`}>
+                        <XMarkIcon className='w-5 h-5' />
+                      </button>
+                    </div>
+                  )
                 }
                 return null
               })}
