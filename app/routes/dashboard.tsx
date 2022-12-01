4134717ffa5559b489f834b1ea8254f591108d50
Juan D. Jara <juanorigami@gmail.com>
2022-12-01 01:39:28 +0100

    remove redirection from dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,13 +1,14 @@
 import Container from '@/components/Container'
 import PostList from '@/components/post/PostList'
 import type { Post } from '@/lib/api.server'
-import { getUserRelations } from '@/lib/api.server'
 import { getDashboard } from '@/lib/api.server'
-import { requireUserSession, setFlashMessage } from '@/lib/session.server'
+import { requireUserSession } from '@/lib/session.server'
+import { linkCN } from '@/lib/style'
+import useUser from '@/lib/useUser'
+import useUserRelations from '@/lib/useUserRelations'
 import type { LoaderFunction} from '@remix-run/node'
-import { redirect } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
 
 type LoaderData = {
   posts: Post[]
@@ -18,19 +19,7 @@ type LoaderData = {
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const { token, user } = await requireUserSession(request)
-  const { followedUsers } = await getUserRelations(token)
-  const followingNoOne = followedUsers.filter(id => id !== user?.userId).length === 0
-  
-  if (followingNoOne) {
-    const cookie = await setFlashMessage(request, 'You are following no one, so we took you to the explore page')
-    throw redirect('/explore', {
-      headers: {
-        'Set-Cookie': cookie
-      }
-    })
-  }
-
+  const { token } = await requireUserSession(request)
   const sp = new URL(request.url).searchParams
   const page = Number(sp.get('page')) || 0
   const startScroll = Number(sp.get('startScroll')) || Date.now()
@@ -42,13 +31,25 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function Dashboard() {
   const { posts, params: { startScroll } } = useLoaderData<LoaderData>()
-
+  const user = useUser()
+  const { followedUsers } = useUserRelations()
+  const followingNoOne = followedUsers.filter(id => id !== user?.userId).length === 0
+  
   return (
     <Container>
       <h1 className='mb-4 text-4xl font-medium text-gray-500'>Dashboard</h1>
+      {followingNoOne && (
+        <div className='mb-2'>
+          <p>
+            You are following no one, so only your posts will be shown here.
+            <br />
+            You can go to <Link className={linkCN} to='/explore'>Explore</Link> to see more posts.
+          </p>
+        </div>
+      )}
       <PostList
         initialPosts={posts}
-        getPageURL={page => `/explore?index&page=${page}&startScroll=${startScroll}`}
+        getPageURL={page => `/dashboard?index&page=${page}&startScroll=${startScroll}`}
       />
     </Container>
   )
