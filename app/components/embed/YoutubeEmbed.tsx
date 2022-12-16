6af15556a58f5962a5e6453e6a0f594a75348da1
Juan D. Jara <juanorigami@gmail.com>
2022-12-16 12:13:13 +0100

    iframe margins

--- app/components/embed/YoutubeEmbed.tsx
+++ app/components/embed/YoutubeEmbed.tsx
@@ -1,10 +1,9 @@
 export default function YoutubeEmbed({ id }: { id: string }) {
   return (
-    <span className="block aspect-w-16 aspect-h-9 rounded-md">
+    <span className="my-1 block aspect-w-16 aspect-h-9 rounded-md">
       <iframe
         width="710"
         height="400"
-        style={{ margin: '8px 0' }}
         className='w-full h-full object-center object-cover rounded-md'
         src={`https://www.youtube-nocookie.com/embed/${id}`}
         title="YouTube video player"
